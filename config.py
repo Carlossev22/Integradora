32d25752724a8150ff2a65eb9e3b1f9508d20c6e
Gema Janneth Ocampo Lopez <105512539+JannLPZ@users.noreply.github.com>
2024-04-17 07:32:35 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,22 +1,14 @@
-from decouple import config
-
 class Config:
-    SECRET_KEY = 'B!1weNAt1T^%kvhUI*S^'
+    SECRET_KEY='B!1weNAt1T^%kvhUI*S^'
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    MYSQL_HOST='coveldb.crpztcwucjwd.us-east-1.rds.amazonaws.com'
-    MYSQL_USER='Sevilla'
-    MYSQL_PASSWORD='Carlos2510'
+    MYSQL_HOST='localhost'
+    MYSQL_USER='root'
+    MYSQL_PASSWORD='123456'
     MYSQL_DB='tienda'
-    MAIL_SERVER='smtp.googlemail.com'
-    MAIL_PORT=587
-    MAIL_USE_TLS=True
-    MAIL_USERNAME='20213tn043@utez.edu.mx'
-    MAIL_PASSWORD=config('MAIL_PASSWORD')
-
 
 config = {
-    'development': DevelopmentConfig,
-    'default': DevelopmentConfig
-}
+     'development':DevelopmentConfig,
+     'defualt':DevelopmentConfig
+ }   
