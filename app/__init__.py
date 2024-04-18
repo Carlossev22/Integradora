b80b3f40b5dda48ed7db5469e8620d0978f3a0d8
Gema Janneth Ocampo Lopez <105512539+JannLPZ@users.noreply.github.com>
2024-04-18 12:29:09 -0600

    Jann
    Janneth

--- app/__init__.py
+++ app/__init__.py
@@ -1,151 +1,39 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, jsonify
-from flask_mysqldb import MySQL
+from flask import Flask, render_template, request, url_for, redirect
 from flask_wtf.csrf import CSRFProtect
-from flask_login import LoginManager, login_user, logout_user, login_required, current_user
-from flask_mail import Mail
-
-from .models.ModeloCompra import ModeloCompra
-from .models.ModeloLibro import ModeloLibro
-from .models.ModeloUsuario import ModeloUsuario
-
-from .models.entities.compra import Compra
-from .models.entities.Libro import Libro
-from .models.entities.Usuario import Usuario
-
-from .consts import *
-from .emails import confirmacion_compra
 
 app = Flask(__name__)
 
 csrf=CSRFProtect()
-db=MySQL(app)
-login_manager_app=LoginManager(app)
-mail=Mail()
 
-@login_manager_app.user_loader
-def load_user(id):
-    return ModeloUsuario.obtener_por_id(db,id)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-
-@app.route('/password/<password>')
-
-
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=(['GET','POST']))
 def login():
+    """
+    print(request.method)
+    print(request.form['usuario'])
+    print(request.form['password'])
+    """
+    #csfr (cross-site Request forgery):solicitud de falsificacion entre sitios
     if request.method=='POST':
-         usuario=Usuario(None, request.form['usuario'], request.form['password'], None)
-         usuario_logeado=ModeloUsuario.login(db,usuario)
-         if usuario_logeado != None:
-             login_user(usuario_logeado)
-             flash(MENSAJE_BIENVENIDA, 'success')
-             return redirect(url_for('index'))
-         else:
-            flash(LOGIN_CREDENCIALESINVALIDAS, 'warning')
+       #print(request.form['usuario'])
+       #print(request.form['password'])
+        if request.form['usuario'] == 'admin1' and request.form['password']=='123456':
+            return redirect(url_for('index'))
+        else:
             return render_template('auth/login.html')
     else:
         return render_template('auth/login.html')
     
-@app.route('/logout')
-def logout():
-    logout_user()
-    flash(LOGOUT, 'success')
-    return redirect(url_for('login'))
-
-@app.route('/')
-@login_required
-def index():
-    if current_user.is_authenticated:
-        if current_user.tipousuario.id==1:
-            try:
-                libros_vendidos=ModeloLibro.listar_libros_vendidos(db)
-                data={
-                    'titulo':'Libros Vendidos',
-                    'libros_vendidos':libros_vendidos
-                }
-                return render_template('index.html',data=data)
-            except Exception as ex:
-                return render_template('errores/error.html', mensaje=format(ex))
-        else:
-            try:
-                compras= ModeloCompra.listar_compras_usuario(db, current_user)
-                data={
-                    'titulo':'Mis compras',
-                    'compras':compras
-                }
-                return render_template('index.html',data=data)
-            except Exception as ex:
-                return render_template('errores/error.html', mensaje=format(ex))
-        
-    else:
-        return redirect(url_for('login'))
-    
-@app.route('/libros')
-@login_required
-def listar_libros():
-    try:
-        libros = ModeloLibro.listar_libros(db)
-        data={
-            'titulo':'listado de libros',
-            "libros":libros
-        }
-        return render_template('listado_libros.html', data=data)
-    except Exception as ex:
-        return render_template('errores/error.html', mensaje=format(ex))
-    
-@app.route('/comprarLibro', methods=['POST'])
-@login_required
-def comprar_libro():
-    data_request = request.get_json()
-    data={}
-    try:
-        # libro = Libro(data_request['isbn'],None, None, None, None)
-        libro = ModeloLibro.leer_libro(db, data_request['isbn'])
-        compra= Compra(None, libro, current_user)
-        data['exito']= ModeloCompra.registrar_compra(db, compra)
-        # confirmacion_compra(mail, current_user, libro)
-        confirmacion_compra(app, mail, current_user, libro)
-    except Exception as ex:
-        data['mensaje']=format(ex)
-        data['exito']= False
-    return jsonify(data)
-
-@app.route('/registro', methods=['GET', 'POST'])
-def registro():
-    form = MyForm()
-    print(form)  # Imprime el contenido del formulario en la consola
-    if form.validate_on_submit():
-        # Procesar los datos del formulario aquí
-        return 'Formulario enviado con éxito!'
-    return render_template('formulario.html', form=form)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        nombre_completo = request.form['nombre_completo']
-        direccion = request.form['direccion']
-        correo = request.form['correo']
-        telefono = request.form['telefono']
-        usuario = request.form['usuario']
-        password = request.form['password']
-
-        nuevo_usuario = Usuario(nombre_completo=nombre_completo, direccion=direccion, correo=correo, telefono=telefono, usuario=usuario, password=password)
-        db.session.add(nuevo_usuario)
-        db.session.commit()
-        return redirect(url_for('login'))
-    else:
-        return render_template('auth/formulario.html')
-
 def pagina_no_encontrada(error):
-    return render_template('errores/404.html') 
+    return render_template('errores/404.html'), 404
 
-def pagina_no_autorizada(error):
-    return redirect(url_for('login'))
 
 def inicializar_app(config):
     app.config.from_object(config)
     csrf.init_app(app)
-    mail.init_app(app)
-    app.register_error_handler(401, pagina_no_autorizada)
     app.register_error_handler(404, pagina_no_encontrada)
     return app
